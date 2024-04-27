3d2542e55751a78036a1837390a6fd78a8d78f4f
Isuru Adithya Sandaruwan <isuru1999adi.sandaru@gmail.com>
2024-04-27 09:58:38 +0530

    front-end code initial setup

---
+++ frontend/src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './descriptionBox.css'
+
+const DescriptionBox = () => {
+  return (
+    <div className='description-box'>
+<div className="descriptionbox-navigator">
+    <div className="descriptionbox-nav-box">Description</div>
+    <div className="descriptionbox-nav-box fade">
+        Reviewss (122)
+    </div>
+</div>
+<div className="descriptionbox-description">
+    <p>
+An e-commerce web application is a digital platform that enables businesses and individuals to buy and sell goods and services online. These applications offer a user-friendly interface for browsing, selecting, and purchasing products, often featuring secure payment options and efficient shipping and delivery processes. E-commerce web apps also provide tools for managing orders, tracking shipments, and offering customer support. Many include features such as product recommendations and reviews to enhance the shopping experience for users. Overall, e-commerce web applications have revolutionized the way commerce is conducted, offering convenience and accessibility to customers worldwide.</p>
+<p></p>
+</div>
+    </div>
+  )
+}
+
+export default DescriptionBox
